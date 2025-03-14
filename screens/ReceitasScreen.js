d159cc6fd3460b105694cf9fce5d1c7fa7ba4cd4
Luiz Filipi <luizfilipi3246@gmail.com>
2025-03-14 17:18:53 -0300

    mais opções de receitas

--- screens/ReceitasScreen.js
+++ screens/ReceitasScreen.js
@@ -14,6 +14,8 @@ export default function ReceitasScreen({ navigation }) {
         { id: 1, imagem: require('../assets/boloChocolate.jpg'), nome: 'Bolo de Chocolate', ingredientes: 'Farinha, Ovos, Chocolate...', preparo: 'Misture tudo e asse por 40min.' },
         { id: 2, imagem: require('../assets/panqueca.jpg'), nome: 'Panqueca', ingredientes: 'Farinha, Leite, Ovos...', preparo: 'Misture tudo e frite em uma frigideira.' },
         { id: 3, imagem: require('../assets/brigadeiro.jpg'), nome: 'Brigadeiro', ingredientes: 'Leite Condensado, Chocolate, Manteiga...', preparo: 'Cozinhe até engrossar e enrole.' },
+        { id: 4, imagem: require('../assets/pizza.jpg'), nome: 'Pizza', ingredientes: 'Farinha, Fermento, Calabresa...', preparo: 'Misture farinha e fermento, adicione calabresa e asse pro 30min' },
+        { id: 5, imagem: require('../assets/tacos.jpg'), nome: 'Tacos', ingredientes: 'Tortilha, Guacamole, Tomate...', preparo: 'Adicione tudo na tortilha e asse' },
     ];
 
     const filteredReceitas = receitas.filter((receita) =>
@@ -29,8 +31,7 @@ export default function ReceitasScreen({ navigation }) {
                 placeholder="Buscar Receita"
                 placeholderTextColor="#aaa"
                 value={searchQuery}
-                onChangeText={setSearchQuery}
-            />
+                onChangeText={setSearchQuery} />
 
             <FlatList
                 data={filteredReceitas}
